48f2040f338428e55f3ba7e08b8f84c4f16c2a46
jonatanjrss <jonatanjrss@gmail.com>
2022-07-05 10:31:13 -0300

    implementa password change done

--- ars/authentication/urls.py
+++ ars/authentication/urls.py
@@ -13,4 +13,5 @@ urlpatterns = [
     path('reset/<uidb64>/<token>/', views.CustomPasswordResetConfirmView.as_view(), name='password_reset_confirm'),
     path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
     path('password_change/', auth_views.PasswordChangeView.as_view(), name='password_change'),
+    path('password_change_done/', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
 ]
